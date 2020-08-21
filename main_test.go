bcef906c1ed6555151ceca52bf8f5d0d7df495f2
Ben Duncan <bennydee@amazon.com>
2020-08-21 13:43:13 +1000

    Revert

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSayHello(t *testing.T) {
-	want := "Hola world!"
+	want := "Hello world!"
 	if got := SayHello(); got != want {
 		t.Errorf("SayHello() = %q, want %q", got, want)
 	}
